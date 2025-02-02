7c18261f43a249d0408944697fd2ec6c5666e2ad
Paulina Liwanag <liwanag.paulina@gmail.com>
2025-02-01 20:34:00 -0500

    add some todos, add potential background, not sure if i want it though

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,14 +11,24 @@ const vinyls = [
   { id: 3, title: "Contact", videoId: process.env.REACT_APP_VIDEO_3 }
 ];
 
+// todo: make usable on mobile browser**
+
 const App = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <div style={{ textAlign: "center", padding: "20px" }}>
         {/* <h1>drag and drop a record onto the player to learn more about me!</h1> */}
         <div className="vinyl-menu" style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-          {vinyls.map((vinyl) => (
-            <Vinyl key={vinyl.id} id={vinyl.id} title={vinyl.title} videoId={vinyl.videoId} dragged />
+          {/**
+           * **todo: add double click functionality
+           */}
+          {vinyls.map((vinyl, index) => (
+            <Vinyl
+              key={index}
+              title={vinyl.title}
+              videoId={vinyl.videoId}
+            // onDoubleClick={(videoId, title) => handleTrackDrop(videoId, title)}
+            />
           ))}
         </div>
         <RecordPlayer />
